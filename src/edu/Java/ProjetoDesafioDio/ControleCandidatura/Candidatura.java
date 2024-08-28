397ee642cd5211b205d2cac7af3c402e709fbb4d
Parvus <natan.guimaraes2003@gmail.com>
2024-08-28 15:46:56 -0300

    ajuste de pastas e criação do esqueleto do processo de candidatura

---
+++ src/edu/Java/ProjetoDesafioDio/ControleCandidatura/Candidatura.java
@@ -0,0 +1,26 @@
+package ControleCandidatura;
+
+import java.util.Scanner;
+
+
+public class Candidatura{
+    
+    public static void main(String[] args) {
+
+        System.out.println("processo de candidatura");
+        System.out.println("Insira a pretenção salarial: ");
+        analisarCandidatos(0);
+        
+    }
+
+static void analisarCandidatos(double SalarioPretendido){
+    Scanner sc = new Scanner(System.in);
+    double SalarioBase = 2000;
+    SalarioPretendido = sc.nextDouble();
+
+    
+    
+
+    }
+
+}
