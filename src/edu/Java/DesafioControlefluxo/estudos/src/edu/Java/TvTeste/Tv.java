ad1e9f803639db50aa4bc5ccccd1938055b14533
Parvus <89171265+3parvus@users.noreply.github.com>
2024-09-17 01:26:40 -0300

    Add files via upload

---
+++ src/edu/Java/DesafioControlefluxo/estudos/src/edu/Java/TvTeste/Tv.java
@@ -0,0 +1,19 @@
+public class Tv {
+    boolean estadoTv = false;
+    int Volume = 25;
+    int Canal = 1;
+
+    public boolean estadoTv(boolean estado){
+        estado = estadoTv ?  true :  false;
+        return estadoTv;
+    }
+    public int aumentarVolume(int Volume){        
+       Volume += Volume;
+        return Volume;
+    }
+    public int diminuirVolume(int Volume){
+        Volume -=Volume;
+        return Volume;
+    }
+    
+}
